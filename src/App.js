72aebd6994a7352470448e43cdcfd60b76ebc0fb
akkiadepu <akki.adepu@gmail.com>
2024-03-27 13:42:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import DoctorPage from './DoctorPage';
+import PatientPage from './PatientPage';
+// import Home from './Home';
 import './App.css';
+import AllPatientsTable from './AllPatientsTable';
+import Navbar from "./Navbar";
+
+
+function Home() {
+  return <div><h2>Home </h2>
+
+<AllPatientsTable backendUrl="http://restdemo01-env.eba-sdpzxuex.eu-north-1.elasticbeanstalk.com" />
+</div>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className='App'>
+        {/* <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="container">
+          <header id="logo">Trust<span>Cure+</span></header>
+            <div className="collapse navbar-collapse">
+              <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                  <Link to="/" className="nav-link">Home</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/doctors" className="nav-link">Doctors</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/patients" className="nav-link">Patients</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav> */}
+
+
+<Navbar/>
+   
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/doctors" element={<DoctorPage />} />
+          <Route path="/patients" element={<PatientPage/>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
